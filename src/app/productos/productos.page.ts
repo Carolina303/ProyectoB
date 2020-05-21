5f5dfdd675e9ea4fbfb90f442769a10297767ad8
SusanaEnriquez <susyenriquez76@gmail.com>
2020-05-21 15:10:15 -0500

    Productos

--- src/app/productos/productos.page.ts
+++ src/app/productos/productos.page.ts
@@ -6,7 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productos.page.scss'],
 })
 export class ProductosPage implements OnInit {
+  slides = [
+    {
+      s: 'Sembradio #1',
+      img: 'assets/papa.jpeg',
+      titulo: '¡Contamos con 10 hectareas de sembradio a diferente tiempo para la cosecha!'
+    },
+    {
+      s: 'Sembradio #1',
+      img: 'assets/maiz.jpeg',
+      titulo: '¡Contamos con 10 hectareas de sembradio a diferente tiempo para la cosecha!'
+    },
+    {
+      s: 'Sembradio #1',
+      img: 'assets/frijol.jpeg',
+      titulo: '¡Contamos con 10 hectareas de sembradio a diferente tiempo para la cosecha!'
+    },
+    {
+      s: 'Sembradio #2',
+      img: 'assets/melon.jpeg',
+      titulo: '¡Contamos con 10 hectareas de sembradio a diferente tiempo para la cosecha!'
+    },
+    {
+      s: 'Sembradio #2',
+      img: 'assets/sandia.jpeg',
+      titulo: '¡Contamos con 10 hectareas de sembradio a diferente tiempo para la cosecha!'
+    },
+    {
+      s: 'Sembradio #2',
+      img: 'assets/naranjas.jpeg',
+      titulo: '¡Contamos con 10 hectareas de sembradio a diferente tiempo para la cosecha!'
+    },
 
+  ]
   constructor() { }
 
   ngOnInit() {
